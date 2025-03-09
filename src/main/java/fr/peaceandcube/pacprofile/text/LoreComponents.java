0ee469475cbd43b1b17ef4a81dfdca41d38c2814
YanisBft <yanis.briffaut@gmail.com>
2025-03-09 19:00:07 +0100

    Clean some duplicate code

--- src/main/java/fr/peaceandcube/pacprofile/text/LoreComponents.java
+++ src/main/java/fr/peaceandcube/pacprofile/text/LoreComponents.java
@@ -6,108 +6,112 @@ import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 
 public class LoreComponents {
-    public static Component PROFILE_RANK = Component.text(Messages.PROFILE_RANK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component PROFILE_RANK_EXPIRATION = Component.text(Messages.PROFILE_RANK_EXPIRATION, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component PROFILE_NICKNAME = Component.text(Messages.PROFILE_NICKNAME, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component PROFILE_BIRTHDAY = Component.text(Messages.PROFILE_BIRTHDAY, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component PROFILE_JOIN_DATE = Component.text(Messages.PROFILE_JOIN_DATE, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
+    public static Component PROFILE_RANK;
+    public static Component PROFILE_RANK_EXPIRATION;
+    public static Component PROFILE_NICKNAME;
+    public static Component PROFILE_BIRTHDAY;
+    public static Component PROFILE_JOIN_DATE;
 
-    public static Component STATISTICS_BASE = Component.text(Messages.STATISTICS_BASE, TextColor.color(0xFF5555)).decoration(TextDecoration.ITALIC, false);
-    public static Component STATISTICS_CURRENT = Component.text(Messages.STATISTICS_CURRENT, TextColor.color(0x55FF55)).decoration(TextDecoration.ITALIC, false);
-    public static Component STATISTICS_HEALTH = Component.text(Messages.STATISTICS_HEALTH, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component STATISTICS_MAX_HEALTH = Component.text(Messages.STATISTICS_MAX_HEALTH, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component STATISTICS_ARMOR = Component.text(Messages.STATISTICS_ARMOR, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component STATISTICS_ARMOR_TOUGHNESS = Component.text(Messages.STATISTICS_ARMOR_TOUGHNESS, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component STATISTICS_KNOCKBACK_RESISTANCE = Component.text(Messages.STATISTICS_KNOCKBACK_RESISTANCE, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component STATISTICS_SPEED = Component.text(Messages.STATISTICS_SPEED, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component STATISTICS_ATTACK_DAMAGE = Component.text(Messages.STATISTICS_ATTACK_DAMAGE, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component STATISTICS_ATTACK_SPEED = Component.text(Messages.STATISTICS_ATTACK_SPEED, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component STATISTICS_LUCK = Component.text(Messages.STATISTICS_LUCK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component STATISTICS_CLICK_BASE = Component.text(Messages.STATISTICS_CLICK_BASE, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component STATISTICS_CLICK_CURRENT = Component.text(Messages.STATISTICS_CLICK_CURRENT, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
+    public static Component STATISTICS_BASE;
+    public static Component STATISTICS_CURRENT;
+    public static Component STATISTICS_HEALTH;
+    public static Component STATISTICS_MAX_HEALTH;
+    public static Component STATISTICS_ARMOR;
+    public static Component STATISTICS_ARMOR_TOUGHNESS;
+    public static Component STATISTICS_KNOCKBACK_RESISTANCE;
+    public static Component STATISTICS_SPEED;
+    public static Component STATISTICS_ATTACK_DAMAGE;
+    public static Component STATISTICS_ATTACK_SPEED;
+    public static Component STATISTICS_LUCK;
+    public static Component STATISTICS_CLICK_BASE;
+    public static Component STATISTICS_CLICK_CURRENT;
 
-    public static Component SETTINGS_CLICK = Component.text(Messages.SETTINGS_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component SETTINGS_ENABLED = Component.text(Messages.SETTINGS_ENABLED, TextColor.color(0x55FF55), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component SETTINGS_DISABLED = Component.text(Messages.SETTINGS_DISABLED, TextColor.color(0xFF5555), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component SETTINGS_MSGTOGGLE_CLICK = Component.text(Messages.SETTINGS_MSGTOGGLE_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component SETTINGS_TOGGLEMSGSOUND_CLICK = Component.text(Messages.SETTINGS_TOGGLEMSGSOUND_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component SETTINGS_PTIME_CLICK_LEFT = Component.text(Messages.SETTINGS_PTIME_CLICK_LEFT, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component SETTINGS_PTIME_CLICK_RIGHT = Component.text(Messages.SETTINGS_PTIME_CLICK_RIGHT, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component SETTINGS_PWEATHER_CLICK_LEFT = Component.text(Messages.SETTINGS_PWEATHER_CLICK_LEFT, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component SETTINGS_PWEATHER_CLICK_RIGHT = Component.text(Messages.SETTINGS_PWEATHER_CLICK_RIGHT, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
+    public static Component SETTINGS_CLICK;
+    public static Component SETTINGS_ENABLED;
+    public static Component SETTINGS_DISABLED;
+    public static Component SETTINGS_MSGTOGGLE_CLICK;
+    public static Component SETTINGS_TOGGLEMSGSOUND_CLICK;
+    public static Component SETTINGS_PTIME_CLICK_LEFT;
+    public static Component SETTINGS_PTIME_CLICK_RIGHT;
+    public static Component SETTINGS_PWEATHER_CLICK_LEFT;
+    public static Component SETTINGS_PWEATHER_CLICK_RIGHT;
 
-    public static Component COINS_NUMBER = Component.text(Messages.COINS_NUMBER, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component COINS_CLICK = Component.text(Messages.COINS_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
+    public static Component COINS_NUMBER;
+    public static Component COINS_CLICK;
 
-    public static Component HEAD_TICKETS_NUMBER = Component.text(Messages.HEAD_TICKETS_NUMBER, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component HEAD_TICKETS_CLICK = Component.text(Messages.HEAD_TICKETS_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
+    public static Component HEAD_TICKETS_NUMBER;
+    public static Component HEAD_TICKETS_CLICK;
 
-    public static Component MAILS_TOTAL = Component.text(Messages.MAILS_TOTAL, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component MAILS_UNREAD = Component.text(Messages.MAILS_UNREAD, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component MAILS_CLICK = Component.text(Messages.MAILS_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
+    public static Component MAILS_TOTAL;
+    public static Component MAILS_UNREAD;
+    public static Component MAILS_CLICK;
 
-    public static Component HOMES_TOTAL = Component.text(Messages.HOMES_TOTAL, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component HOMES_REMAINING = Component.text(Messages.HOMES_REMAINING, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component HOMES_MAX_AVAILABLE = Component.text(Messages.HOMES_MAX_AVAILABLE, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component HOMES_CLICK = Component.text(Messages.HOMES_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component HOME_WORLD = Component.text(Messages.HOME_WORLD, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component HOME_X = Component.text(Messages.HOME_X, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component HOME_Y = Component.text(Messages.HOME_Y, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component HOME_Z = Component.text(Messages.HOME_Z, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component HOME_CLICK_LEFT = Component.text(Messages.HOME_CLICK_LEFT, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component HOME_CLICK_RIGHT = Component.text(Messages.HOME_CLICK_RIGHT, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component HOME_NOTES_CLICK_LEFT = Component.text(Messages.HOME_NOTES_CLICK_LEFT, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component HOME_NOTES_CLICK_RIGHT = Component.text(Messages.HOME_NOTES_CLICK_RIGHT, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component HOME_COLOR_CLICK = Component.text(Messages.HOME_COLOR_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
+    public static Component HOMES_TOTAL;
+    public static Component HOMES_REMAINING;
+    public static Component HOMES_MAX_AVAILABLE;
+    public static Component HOMES_CLICK;
+    public static Component HOME_WORLD;
+    public static Component HOME_X;
+    public static Component HOME_Y;
+    public static Component HOME_Z;
+    public static Component HOME_CLICK_LEFT;
+    public static Component HOME_CLICK_RIGHT;
+    public static Component HOME_NOTES_CLICK_LEFT;
+    public static Component HOME_NOTES_CLICK_RIGHT;
+    public static Component HOME_COLOR_CLICK;
 
-    public static Component CLAIMS_TOTAL = Component.text(Messages.CLAIMS_TOTAL, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIMS_CB_USED = Component.text(Messages.CLAIMS_CB_USED, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIMS_CB_REMAINING = Component.text(Messages.CLAIMS_CB_REMAINING, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIMS_CB_ACCRUED = Component.text(Messages.CLAIMS_CB_ACCRUED, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIMS_CB_BONUS = Component.text(Messages.CLAIMS_CB_BONUS, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIMS_CB_TOTAL = Component.text(Messages.CLAIMS_CB_TOTAL, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIMS_CB_PER_HOUR = Component.text(Messages.CLAIMS_CB_PER_HOUR, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIMS_CLICK = Component.text(Messages.CLAIMS_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIM_WORLD = Component.text(Messages.CLAIM_WORLD, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIM_GREATER_CORNER = Component.text(Messages.CLAIM_GREATER_CORNER, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIM_LESSER_CORNER = Component.text(Messages.CLAIM_LESSER_CORNER, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIM_WIDTH = Component.text(Messages.CLAIM_WIDTH, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIM_LENGTH = Component.text(Messages.CLAIM_LENGTH, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIM_AREA = Component.text(Messages.CLAIM_AREA, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIM_PERMISSIONS_BUILDERS = Component.text(Messages.CLAIM_PERMISSIONS_BUILDERS, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIM_PERMISSIONS_CONTAINERS = Component.text(Messages.CLAIM_PERMISSIONS_CONTAINERS, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIM_PERMISSIONS_ACCESSORS = Component.text(Messages.CLAIM_PERMISSIONS_ACCESSORS, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIM_PERMISSIONS_MANAGERS = Component.text(Messages.CLAIM_PERMISSIONS_MANAGERS, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIM_NAME_CLICK = Component.text(Messages.CLAIM_NAME_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
+    public static Component CLAIMS_TOTAL;
+    public static Component CLAIMS_CB_USED;
+    public static Component CLAIMS_CB_REMAINING;
+    public static Component CLAIMS_CB_ACCRUED;
+    public static Component CLAIMS_CB_BONUS;
+    public static Component CLAIMS_CB_TOTAL;
+    public static Component CLAIMS_CB_PER_HOUR;
+    public static Component CLAIMS_CLICK;
+    public static Component CLAIM_WORLD;
+    public static Component CLAIM_GREATER_CORNER;
+    public static Component CLAIM_LESSER_CORNER;
+    public static Component CLAIM_WIDTH;
+    public static Component CLAIM_LENGTH;
+    public static Component CLAIM_AREA;
+    public static Component CLAIM_PERMISSIONS_BUILDERS;
+    public static Component CLAIM_PERMISSIONS_CONTAINERS;
+    public static Component CLAIM_PERMISSIONS_ACCESSORS;
+    public static Component CLAIM_PERMISSIONS_MANAGERS;
+    public static Component CLAIM_NAME_CLICK;
 
-    public static Component ONLINE_PLAYERS_COUNT = Component.text(Messages.ONLINE_PLAYERS_COUNT, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component ONLINE_PLAYERS_CLICK = Component.text(Messages.ONLINE_PLAYERS_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component ONLINE_PLAYER_TRUST_COUNT_1 = Component.text(Messages.ONLINE_PLAYER_TRUST_COUNT_1, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component ONLINE_PLAYER_TRUST_COUNT_2 = Component.text(Messages.ONLINE_PLAYER_TRUST_COUNT_2, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component ONLINE_PLAYER_CLICK = Component.text(Messages.ONLINE_PLAYER_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component ONLINE_PLAYER_NOTES_CLICK_LEFT = Component.text(Messages.ONLINE_PLAYER_NOTES_CLICK_LEFT, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component ONLINE_PLAYER_NOTES_CLICK_RIGHT = Component.text(Messages.ONLINE_PLAYER_NOTES_CLICK_RIGHT, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
+    public static Component ONLINE_PLAYERS_COUNT;
+    public static Component ONLINE_PLAYERS_CLICK;
+    public static Component ONLINE_PLAYER_TRUST_COUNT_1;
+    public static Component ONLINE_PLAYER_TRUST_COUNT_2;
+    public static Component ONLINE_PLAYER_CLICK;
+    public static Component ONLINE_PLAYER_NOTES_CLICK_LEFT;
+    public static Component ONLINE_PLAYER_NOTES_CLICK_RIGHT;
 
-    public static Component WARPS_CLICK = Component.text(Messages.WARPS_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component WARP_COMMAND = Component.text(Messages.WARP_COMMAND, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component WARP_CATEGORY = Component.text(Messages.WARP_CATEGORY, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component WARP_CLICK = Component.text(Messages.WARP_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
+    public static Component WARPS_CLICK;
+    public static Component WARP_COMMAND;
+    public static Component WARP_CATEGORY;
+    public static Component WARP_CLICK;
 
-    public static Component RULES_CLICK = Component.text(Messages.RULES_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
+    public static Component RULES_CLICK;
 
-    public static Component LINKS_CLICK = Component.text(Messages.LINKS_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
+    public static Component LINKS_CLICK;
 
-    public static Component DYNMAP_CLICK = Component.text(Messages.DYNMAP_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
+    public static Component DYNMAP_CLICK;
 
-    public static Component ORDER_BY = Component.text(Messages.ORDER_BY, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
-    public static Component ORDER_DEFAULT = Component.text(Messages.ORDER_DEFAULT, TextColor.color(0xFFFF55), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component ORDER_NAME_AZ = Component.text(Messages.ORDER_NAME_AZ, TextColor.color(0xFFFF55), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component ORDER_NAME_ZA = Component.text(Messages.ORDER_NAME_ZA, TextColor.color(0xFFFF55), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component ORDER_AREA_ASC = Component.text(Messages.ORDER_AREA_ASC, TextColor.color(0xFFFF55), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component ORDER_AREA_DESC = Component.text(Messages.ORDER_AREA_DESC, TextColor.color(0xFFFF55), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component ORDER_CATEGORY_AZ = Component.text(Messages.ORDER_CATEGORY_AZ, TextColor.color(0xFFFF55), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component ORDER_CATEGORY_ZA = Component.text(Messages.ORDER_CATEGORY_ZA, TextColor.color(0xFFFF55), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component ORDER_CLICK = Component.text(Messages.ORDER_CLICK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
+    public static Component ORDER_BY;
+    public static Component ORDER_DEFAULT;
+    public static Component ORDER_NAME_AZ;
+    public static Component ORDER_NAME_ZA;
+    public static Component ORDER_AREA_ASC;
+    public static Component ORDER_AREA_DESC;
+    public static Component ORDER_CATEGORY_AZ;
+    public static Component ORDER_CATEGORY_ZA;
+    public static Component ORDER_CLICK;
+
+    static {
+        init();
+    }
 
     public static void init() {
         PROFILE_RANK = Component.text(Messages.PROFILE_RANK, TextColor.color(0xAAAAAA)).decoration(TextDecoration.ITALIC, false);
--- src/main/java/fr/peaceandcube/pacprofile/text/NameComponents.java
+++ src/main/java/fr/peaceandcube/pacprofile/text/NameComponents.java
@@ -6,41 +6,45 @@ import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 
 public class NameComponents {
-    public static Component STATISTICS = Component.text(Messages.STATISTICS, TextColor.color(0xFF55FF), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component SETTINGS = Component.text(Messages.SETTINGS, TextColor.color(0x555555), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component COINS = Component.text(Messages.COINS, TextColor.color(0xFFAA00), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component HEAD_TICKETS = Component.text(Messages.HEAD_TICKETS, TextColor.color(0x00AAAA), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component MAILS = Component.text(Messages.MAILS, TextColor.color(0xAA00AA), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component HOMES = Component.text(Messages.HOMES, TextColor.color(0x5555FF), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIMS = Component.text(Messages.CLAIMS, TextColor.color(0x00AA00), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component ONLINE_PLAYERS = Component.text(Messages.ONLINE_PLAYERS, TextColor.color(0x55FF55), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component WARPS = Component.text(Messages.WARPS, TextColor.color(0xFFFF55), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component RULES = Component.text(Messages.RULES, TextColor.color(0xFF55FF), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component LINKS = Component.text(Messages.LINKS, TextColor.color(0xFF55FF), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component DYNMAP = Component.text(Messages.DYNMAP, TextColor.color(0xFF55FF), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
+    public static Component STATISTICS;
+    public static Component SETTINGS;
+    public static Component COINS;
+    public static Component HEAD_TICKETS;
+    public static Component MAILS;
+    public static Component HOMES;
+    public static Component CLAIMS;
+    public static Component ONLINE_PLAYERS;
+    public static Component WARPS;
+    public static Component RULES;
+    public static Component LINKS;
+    public static Component DYNMAP;
 
-    public static Component EXIT = Component.text(Messages.EXIT, TextColor.color(0xFF5555), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
+    public static Component EXIT;
 
-    public static Component PAGE_PREVIOUS = Component.text(Messages.PAGE_PREVIOUS, TextColor.color(0xFF55FF), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component PAGE_NEXT = Component.text(Messages.PAGE_NEXT, TextColor.color(0xFF55FF), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
+    public static Component PAGE_PREVIOUS;
+    public static Component PAGE_NEXT;
 
-    public static Component CONFIRMATION_YES = Component.text(Messages.CONFIRMATION_YES, TextColor.color(0x55FF55), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component CONFIRMATION_NO = Component.text(Messages.CONFIRMATION_NO, TextColor.color(0xFF5555), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
+    public static Component CONFIRMATION_YES;
+    public static Component CONFIRMATION_NO;
 
-    public static Component SETTINGS_MSGTOGGLE = Component.text(Messages.SETTINGS_MSGTOGGLE, TextColor.color(0x5555FF), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component SETTINGS_TOGGLEMSGSOUND = Component.text(Messages.SETTINGS_TOGGLEMSGSOUND, TextColor.color(0x5555FF), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component SETTINGS_PTIME = Component.text(Messages.SETTINGS_PTIME, TextColor.color(0x5555FF), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component SETTINGS_PWEATHER = Component.text(Messages.SETTINGS_PWEATHER, TextColor.color(0x5555FF), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
+    public static Component SETTINGS_MSGTOGGLE;
+    public static Component SETTINGS_TOGGLEMSGSOUND;
+    public static Component SETTINGS_PTIME;
+    public static Component SETTINGS_PWEATHER;
 
-    public static Component HOMES_ORDER = Component.text(Messages.HOMES_ORDER, TextColor.color(0x00AA00), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component HOME_NOTES = Component.text(Messages.HOME_NOTES, TextColor.color(0x00AA00), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component HOME_COLOR = Component.text(Messages.HOME_COLOR, TextColor.color(0x00AAAA), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
+    public static Component HOMES_ORDER;
+    public static Component HOME_NOTES;
+    public static Component HOME_COLOR;
 
-    public static Component CLAIMS_ORDER = Component.text(Messages.CLAIMS_ORDER, TextColor.color(0x00AA00), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIM_PERMISSIONS = Component.text(Messages.CLAIM_PERMISSIONS, TextColor.color(0x00AA00), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
-    public static Component CLAIM_NAME = Component.text(Messages.CLAIM_NAME, TextColor.color(0x00AAAA), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
+    public static Component CLAIMS_ORDER;
+    public static Component CLAIM_PERMISSIONS;
+    public static Component CLAIM_NAME;
 
-    public static Component WARPS_ORDER = Component.text(Messages.WARPS_ORDER, TextColor.color(0x00AA00), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
+    public static Component WARPS_ORDER;
+
+    static {
+        init();
+    }
 
     public static void init() {
         STATISTICS = Component.text(Messages.STATISTICS, TextColor.color(0xFF55FF), TextDecoration.BOLD).decoration(TextDecoration.ITALIC, false);
